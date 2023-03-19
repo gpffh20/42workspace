137e13d4245e13fe9c9994e2fd174596a612bac3
eushin <eunbin584836@gmail.com>
2023-03-19 21:44:56 +0900

    to itoa

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,30 +6,24 @@
 /*   By: eushin <eushin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 16:18:15 by eushin            #+#    #+#             */
-/*   Updated: 2023/03/18 16:25:34 by eushin           ###   ########.fr       */
+/*   Updated: 2023/03/19 16:09:22 by eushin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strdup(const char *s1)
 {
-	int		size;
+	int		i;
 	char	*new_str;
 	char	*tmp;
 
-	size = 0;
 	tmp = (char *)s1;
-	while (tmp[size])
-		size++;
-	new_str = malloc(sizeof(char) * (size + 1));
+	new_str = malloc(sizeof(char) * (ft_strlen(tmp) + 1));
 	if (!new_str)
 		return (NULL);
-	size = 0;
-	while (tmp[size])
-	{
-		new_str[size] = tmp[size];
-		size++;
-	}
-	new_str[size] = 0;
+	i = -1;
+	while (tmp[++i])
+		new_str[i] = tmp[i];
+	new_str[i] = 0;
 	return (new_str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,12 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eushin <eushin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/18 19:38:12 by eushin            #+#    #+#             */
-/*   Updated: 2023/03/18 19:38:47 by eushin           ###   ########.fr       */
+/*   Created: 2023/03/19 19:36:23 by eushin            #+#    #+#             */
+/*   Updated: 2023/03/19 21:39:59 by eushin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char			*res;
+	unsigned int	start;
+	unsigned int	end;
+
+	if (!s1 || !set)
+		return (0);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (start < end && ft_strchr(set, s1[end - 1]))
+		end--;
+	res = ft_substr(s1, start, end - start);
+	return (res);
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%s\n", ft_strtrim("ancde", "123"));
+	return (0);
 }
