89cbd31669607a9997f6213f16b24a16e0e82324
eushin <eunbin584836@gmail.com>
2023-06-27 16:23:48 +0900

    Simplify: ap -> *ap로 재수정 m1에서는 ap로 보내면 실행 안되는 문제 발생해서 다시 고침

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: eushin <eushin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 20:17:14 by eushin            #+#    #+#             */
-/*   Updated: 2023/06/25 21:40:29 by eushin           ###   ########.fr       */
+/*   Updated: 2023/06/27 16:22:19 by eushin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 # define FT_PRINTF_H
 # include <unistd.h>
 # include <stdarg.h>
-# include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
 int		ft_printf(const char *format, ...);
-void	find_format(va_list ap, int *cnt, char format);
+void	find_format(va_list *ap, int *cnt, char format);
 void	print_char(int var, int *cnt);
 void	print_str(char *var, int *cnt);
 void	print_int(int nb, int *cnt);
