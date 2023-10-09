23466f56964c35d92208ac236196635d3b623637
gpffh20 <eunbin584836@gmail.com>
2023-10-09 21:40:42 +0900

    맵 파싱 norm 완료

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -1,40 +1,24 @@
 #include "fdf.h"
 
-void	read_map(int fd)
+int	error_handle(char *str)
 {
-	char	*buff;
-	char	*tmp;
-	int		read_len;
-	char	**map;
-	char	**map_lines;
+	perror(str);
+	return (1);
+}
+
+double	**convert_to_double(char **map, t_fdf *split_info)
+{
 	double	**dmap;
+	char	**map_lines;
 	int		i;
 	int		j;
-	t_fdf	split_info;
 
-	buff = ft_strdup("");
-	tmp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	while (1)
-	{
-		read_len = read(fd, tmp, BUFFER_SIZE);
-		if (read_len <= 0)
-			break ;
-		tmp[read_len] = '\0';
-		buff = ft_strjoin(buff, tmp);
-	}
-	if (!buff)
-	{
-		perror("Error: read error.");
-		exit(1);
-	}
-	free(tmp);
-	map = ft_split(buff, '\n', &split_info);
-	dmap = (double **)malloc(sizeof(double *) * (split_info.row_cnt));
+	dmap = (double **)malloc(sizeof(double *) * (split_info->row_cnt));
 	i = 0;
 	while (map[i])
 	{
-		dmap[i] = (double *)malloc(sizeof(double) * split_info.col_cnt);
-		map_lines = ft_split(map[i], ' ', &split_info);
+		dmap[i] = (double *)malloc(sizeof(double) * split_info->col_cnt);
+		map_lines = ft_split(map[i], ' ', split_info);
 		j = 0;
 		while (map_lines[j])
 		{
@@ -44,19 +28,29 @@ void	read_map(int fd)
 		i++;
 	}
 	free(map);
-	i = 0;
-	while(dmap[i])
+	return (dmap);
+}
+
+double	**read_map(int fd, int read_len)
+{
+	char	*buff;
+	char	tmp[BUFFER_SIZE + 1];
+	char	**map;
+	t_fdf	split_info;
+
+	buff = ft_strjoin("", "");
+	while (1)
 	{
-		j = 0;
-		while(j < 11)
-		{
-			printf("%f ", dmap[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
+		read_len = read(fd, tmp, BUFFER_SIZE);
+		if (read_len <= 0)
+			break ;
+		tmp[read_len] = '\0';
+		buff = ft_strjoin(buff, tmp);
 	}
-	return ;
+	if (!buff)
+		exit(error_handle("Error: read error."));
+	map = ft_split(buff, '\n', &split_info);
+	return (convert_to_double(map, &split_info));
 }
 
 void	right_file(char *title)
@@ -64,31 +58,22 @@ void	right_file(char *title)
 	int	max_idx;
 
 	max_idx = ft_strlen(title);
-	if (ft_strncmp(ft_substr(title, max_idx-4, 4), ".fdf", 4))
+	if (ft_strncmp(ft_substr(title, max_idx - 4, 4), ".fdf", 4))
 		return ;
-	perror("Error: 입력 파일 확장자");
-	exit(1);
+	exit(error_handle("Error: 입력 파일 확장자 에러"));
 }
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-	int	fd;
+	int		fd;
+	double	**dmap;
 
 	if (ac != 2)
-	{
-		perror("Error.");
-		exit(1);
-	}
-
+		exit(error_handle("Error: 인자 수 에러"));
 	right_file(av[1]);
-
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
-	{
-		perror("Error.");
-		exit(1);
-	}
-
-	read_map(fd);
+		exit(error_handle("Error: open 에러"));
+	dmap = read_map(fd, 0);
 	return (0);
 }
