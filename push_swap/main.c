f63ba89abff11771df8aea57bc15d6f3646491f5
gpffh20 <eunbin584836@gmail.com>
2023-11-12 17:54:02 +0900

    compile error catch

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,10 +6,15 @@
 /*   By: eushin <eushin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 14:42:41 by eushin            #+#    #+#             */
-/*   Updated: 2023/11/06 15:15:21 by eushin           ###   ########.fr       */
+/*   Updated: 2023/11/12 17:34:01 by eushin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
+
+void 	a(void)
+{
+	system("leaks push_swap");
+}
 
 void	make_arr(t_deque *a, int *arr)
 {
@@ -28,12 +33,13 @@ void	make_arr(t_deque *a, int *arr)
 
 int	main(int ac, char *av[])
 {
+	atexit(a);
 	t_deque	*a;
 	t_deque	*b;
 	int		*arr;
 
 	if (ac < 2)
-		return ();
+		return (0);
 	a = create_deque();
 	split_args(ac, av, a);
 	arr = (int *) malloc(sizeof(int) * a->size);
