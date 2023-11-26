38983660297fcf824a041cfd9bdfdfc4143abc79
gpffh20 <eunbin584836@gmail.com>
2023-11-27 01:17:16 +0900

    함수 분리 중 백업 용

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -10,50 +10,130 @@ int	error_handle(int errnum)
 		ft_printf("Fork Error!: %s\n", strerror((errno)));
 	else if (errnum == 4)
 		ft_printf("Open Error!: %s\n", strerror(errno));
+	else if (errnum == 5)
+		ft_printf("Dup Error!: %s\n", strerror(errno));
 	exit(1);
+}
+
+char	**get_envpath(char **envp)
+{
+	int 	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", 5))
+			break ;
+		i++;
+	}
+	return (ft_split(envp[i][5], ':');
+}
+
+char	*get_arg(char *cmd1, char **envp)
+{
+	char	**cmd;
+	char	**arg;
+	char 	**path;
+	int 	i;
+
+	i = 0;
+	path = get_envpath(envp);
+	cmd = ft_split(cmd1, ' ');
+	char *cmd_path_tmp = ft_strjoin("/", cmd[0]);
+	while (path[i])
+	{
+		char *cmd_path = ft_strjoin(path[i], cmd_path_tmp);
+		int is_success = access(cmd_path, X_OK);
+		if (is_success == 0)
+		{
+//			free (cmd_path_tmp);
+			break ;
+//			return (cmd_path);
+		}
+		close (is_success);
+		free (cmd_path);
+		i++;
+	}
+	arg = {
+			cmd_path_tmp,
+
+	};
+}
+
+void	exec_child(t_args *args, t_fds *fds, t_exec_args *exec_args)
+{
+	close(args->pipe[0]); // 파이프값을 읽을 일 없으니 바로 닫아줌
+	if (dup2(fds->infile_fd, STDIN_FILENO) < 0)// infile_fd가 stdin fd를 복사 -> infile이 stdin을 가르키게 됨. 즉 stdin하면 infjle에 저장
+		return (error_handle(5));
+	if (dup2(args->pipe[1], STDOUT_FILENO) < 0)	// stdout하면 fd[1]에 저장
+		return (error_handle(5));
+	close(args->pipe[1]);
+	close(fds->infile_fd);
+	execve(exec_args->cmd1, exec_args->arg1, args->envp);
+}
+
+void	exec_parents(t_args *args, t_fds *fds, t_exec_args *exec_args)
+{
+	int status;
+
+	close(args->pipe[1]);
+	if (dup2(args->pipe[0], STDIN_FILENO) < 0)
+		return (error_handle(5));
+	if (dup2(fds->outfile_fd, STDOUT_FILENO) < 0)
+		return (error_handle(5));
+	close(args->pipe[0]);
+	close(fds->outfile_fd);
+	waitpid(args->pid, &status, WNOHANG);
+	execve(exec_args->cmd2, exec_args->arg2, args->envp);
+}
+
+void	init_exec_args()
+{
+
+}
+
+void	execute(t_args *args, t_fds *fds)
+{
+	t_exec_args exec_args;
+
+	init_exec_args();
+	if (pid == 0)
+	{
+		exec_child(args, fds, &exec_args);
+	}
+	else
+	{
+		exec_parents(args, fds, &exec_args);
+	}
+}
+
+void	open_file(t_args *args, t_fds *fds)
+{
+	fds->infile_fd = open(args->av[1], O_RDONLY);
+	fds->outfile_fd = open(args->av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fds->infile_fd < 0 || fds->outfile_fd < 0)
+		return (error_handle(4));
+}
+
+void	init_args(t_args *args, char **av, char **envp)
+{
+	args->av = av;
+	args->envp = envp;
 }
 
 int	main(int ac, char *av[], char *envp[])
 {
+	t_fds	fds;
+	t_args args;
+
 	if (ac != 5)
 		return (error_handle(1));
-
-	int	infile_fd = open(av[1], O_RDONLY);
-	int outfile_fd = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (infile_fd < 0 || outfile_fd < 0)
-		return (error_handle(4));
-
-	int fd[2];
-	if (pipe(fd) < 0)
+	init_args(&args, av, envp);
+	open_file(&args, &fds);
+	if (pipe(args.pipe) < 0)
 		return (error_handle(2));
-	ft_printf("%s\n", p)
-
-	pid_t pid;
-	pid = fork();
-	if (pid < 0)
+	args.pid = fork();
+	if (args.pid < 0)
 		return (error_handle(3));
-	else if (pid == 0)	//	자식 프로세스
-	{
-		close(fd[0]);
-		dup2(infile_fd, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		close(infile_fd);
-		execve(cmd1, arg1, envp);
-	}
-	else	// 부모 프로세스
-	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		dup2(outfile_fd, STDOUT_FILENO);
-		close(fd[0]);
-		close(outfile_fd);
-
-		int status;
-		waitpid(pid, &status, WNOHANG);
-		execve(cmd2, arg2, envp);
-
-	}
-
-
+	execute(&args, &fds);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -9,13 +9,33 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-//typedef struct s_fd
-//{
-//
-//};	t_fd;
-//
-//typedef	struct s_args
-//{
-//
-//}	s_args;
+
+typedef struct s_fds
+{
+	int	infile_fd;
+	int	outfile_fd;
+}	t_fds;
+
+typedef struct s_args
+{
+	char	**av;
+	char	**envp;
+	pid_t	pid;
+	int		pipe[2];
+}	t_args;
+
+typedef struct s_exec_args
+{
+	char	*cmd1;
+	char	*cmd2;
+	char	**arg1;
+	char	**arg2;
+}	t_exec_args;
+
+char	**ft_split(char const *s, char c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+
+
 #endif
