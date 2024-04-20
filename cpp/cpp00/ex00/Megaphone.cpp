262130a8b27b1f010c807453bac0bc433f1b1ee5
gpffh20 <eunbin584836@gmail.com>
2024-04-20 18:20:36 +0900

    chore: cpp convention에 맞게 수정

--- cpp/cpp00/ex00/Megaphone.cpp
+++ cpp/cpp00/ex00/Megaphone.cpp
@@ -2,27 +2,33 @@
 #include <string>
 
 class Megaphone {
-private:
-    char **messages;
+	public:
+		Megaphone(char **messages);
+		~Megaphone();
+		void PrintMessagesToUpperCase(int ac) const;
 
-public:
-    Megaphone(char **messages) : messages(messages) {}
-
-    void PrintMessagesToUpperCase(int ac) const {
-        if (ac == 1) {
-            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-            return;
-        }
-
-        for (int i = 1; i < ac; i++) {
-            std::string message = messages[i];
-			for (size_t j = 0; j < message.length(); j++) {
-				std::cout << (char)std::toupper(message[j]);
-			}
-		}
-		std::cout << std::endl;
-    }
+	private:
+		char **messages;
 };
+
+Megaphone::Megaphone(char **messages) : messages(messages) {}
+
+Megaphone::~Megaphone() {}
+
+void Megaphone::PrintMessagesToUpperCase(int ac) const {
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return;
+	}
+
+	for (int i = 1; i < ac; i++) {
+		std::string message = messages[i];
+		for (size_t j = 0; j < message.length(); j++) {
+			std::cout << static_cast<char>(std::toupper(message[j]));
+		}
+	}
+	std::cout << std::endl;
+}
 
 int main(int ac, char **av) {
     Megaphone megaphone(av);
