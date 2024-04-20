63a12b2cab32b030d6773cdafb130a562f0649ec
gpffh20 <eunbin584836@gmail.com>
2024-04-20 14:36:43 +0900

    feat: ex00 완료 플래그 검사는 안함

--- cpp/cpp00/Megaphone.cpp
+++ cpp/cpp00/Megaphone.cpp
@@ -1,3 +1,31 @@
-//
-// Created by 신은영 on 4/20/24.
-//
+#include <iostream>
+#include <string>
+
+class Megaphone {
+public:
+    Megaphone(char **messages) : messages(messages) {}
+
+    void PrintMessagesToUpperCase(int ac) const {
+        if (ac == 1) {
+            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+            return;
+        }
+
+        for (int i = 1; i < ac; i++) {
+            std::string message = messages[i];
+			for (size_t j = 0; j < message.length(); j++) {
+				std::cout << (char)std::toupper(message[j]);
+			}
+		}
+		std::cout << std::endl;
+    }
+
+private:
+    char **messages;
+};
+
+int main(int ac, char **av) {
+    Megaphone megaphone(av);
+    megaphone.PrintMessagesToUpperCase(ac);
+    return 0;
+}
