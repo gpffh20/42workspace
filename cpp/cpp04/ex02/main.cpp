6e562f2117a7dfaa27ce5b09bc58e0a593cf0386
gpffh20 <eunbin584836@gmail.com>
2024-05-06 21:10:45 +0900

    fix: wronganimal virtual 붙인거 삭제, main수정

--- cpp/cpp04/ex02/main.cpp
+++ cpp/cpp04/ex02/main.cpp
@@ -2,29 +2,20 @@
 #include "Dog.hpp"
 
 int main() {
-    const int size = 10;
 //	AAnimal a1;
-    AAnimal* animals[size];
     std::cout << '\n';
     Cat c1;
     c1.setBrainIdeas(0, "test");
 	Cat c2(c1);
     std::cout << '\n';
-	std::cout << c1.getBrainIdeas(0) << '\n';
-    std::cout << c2.getBrainIdeas(0);
+	std::cout << c1.getBrainIdeas(0) << std::endl;
+    std::cout << c2.getBrainIdeas(0) << std::endl;
+    c2.setBrainIdeas(0, "test2");
+    std::cout << c2.getBrainIdeas(0) << std::endl;
     std::cout << '\n';
 
-    for (int i = 0; i < size / 2; i++) {
-        animals[i] = new Dog();
-    }
-
-    for (int i = size / 2; i < size; i++) {
-        animals[i] = new Cat();
-    }
-
-    for (int i = 0; i < size; i++) {
-        delete animals[i];
-    }
+	c1.makeSound();
+    std::cout << '\n';
 
     return 0;
 }
