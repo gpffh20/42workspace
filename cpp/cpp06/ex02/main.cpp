b57608b16849064c61505de91dfe62bb19eea60d
gpffh20 <eunbin584836@gmail.com>
2024-05-25 01:26:13 +0900

    feat: cpp06의 02완성

---
+++ cpp/cpp06/ex02/main.cpp
@@ -0,0 +1,63 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//void check_leaks() {
+//	system("leaks identify");
+//}
+
+Base* generate() {
+	srand(time(NULL));
+	int random = rand() % 3;
+	switch (random) {
+		case 0:
+			std::cout << "Generated A" << std::endl;
+			return new A();
+		case 1:
+			std::cout << "Generated B" << std::endl;
+			return new B();
+		default:
+			std::cout << "Generated C" << std::endl;
+			return new C();
+	}
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p)) {
+		std::cout << "A" << std::endl;
+	} else if (dynamic_cast<B*>(p)) {
+		std::cout << "B" << std::endl;
+	} else if (dynamic_cast<C*>(p)) {
+		std::cout << "C" << std::endl;
+	}
+}
+
+void identify(Base& p) {
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::bad_cast &e) {}
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	} catch (std::bad_cast &e) {}
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	} catch (std::bad_cast &e) {}
+}
+
+int main() {
+    Base* instance = generate();
+    std::cout << "Identified by pointer: ";
+    identify(instance);
+    std::cout << "Identified by reference: ";
+    identify(*instance);
+    delete instance;
+//	atexit(check_leaks);
+	return 0;
+}
