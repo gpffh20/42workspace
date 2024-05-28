5527871c22fdcd131808ec6793b30fc460347f0f
gpffh20 <eunbin584836@gmail.com>
2024-05-28 23:21:27 +0900

    fix: addManyNumbers 추가

--- cpp/cpp08/ex01/Span.hpp
+++ cpp/cpp08/ex01/Span.hpp
@@ -12,9 +12,24 @@ class Span {
 	Span &operator=(const Span &other);
 	
 	void addNumber(int number);
+	template<typename InputIterator>
+	void addManyNumbers(InputIterator begin, InputIterator end) {
+		if (std::distance(begin, end) + container_.size() > max_num_) {
+			throw std::runtime_error("Container is full");
+		}
+		container_.insert(container_.end(), begin, end);
+	}
+	
+	void print() const {
+		for (std::vector<int>::const_iterator it = container_.begin(); it != container_.end(); ++it) {
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+	}
+	
 	int shortestSpan();
 	int longestSpan();
-	
+  
   private:
 	Span();
 	
--- cpp/cpp08/ex01/main.cpp
+++ cpp/cpp08/ex01/main.cpp
@@ -1,7 +1,9 @@
 #include "Span.hpp"
+#include <stdexcept>
 
 int main() {
 	Span sp = Span(5);
+	Span sp1 = Span(100000);
 	
 	sp.addNumber(6);
 	sp.addNumber(3);
@@ -11,6 +13,26 @@ int main() {
 	
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+	
+	std::cout << "====================================\n" << std::endl;
+	
+	try {
+		Span mySpan(10000);
+		std::vector<int> randVec;
+		
+		srand(static_cast<unsigned int>(time(NULL)));
+		for (int i = 0; i < 10000; ++i)
+			randVec.push_back(rand() % 100000000);
+		
+		mySpan.addManyNumbers(randVec.begin(), randVec.end());
+//		mySpan.print();
+		
+		std::cout << "shortest distance: " << mySpan.shortestSpan() << std::endl;
+		std::cout << "longest distance: " << mySpan.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	
 	return 0;
 }
