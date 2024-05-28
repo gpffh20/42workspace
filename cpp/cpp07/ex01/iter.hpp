73860555de7c8691dd59d027612789ea0a15f840
gpffh20 <eunbin584836@gmail.com>
2024-05-28 15:04:11 +0900

    chore: 불필요하게 선언되어 있는 typename F 삭제

--- cpp/cpp07/ex01/iter.hpp
+++ cpp/cpp07/ex01/iter.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-template <typename T, typename F>
+template <typename T>
 void iter(T *array, size_t length, void (*func)(T const &)) {
 	for (size_t i = 0; i < length; i++) {
 		func(array[i]);
