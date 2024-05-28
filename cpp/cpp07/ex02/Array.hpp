14437afd45c14999dff21185eb29c80ba0a680ee
gpffh20 <eunbin584836@gmail.com>
2024-05-28 15:04:49 +0900

    chore: 불필요한 공백 삭제

--- cpp/cpp07/ex02/Array.hpp
+++ cpp/cpp07/ex02/Array.hpp
@@ -7,19 +7,15 @@ template<typename T>
 class Array {
   public:
 	Array() : array_(NULL), size_(0) {}
-	
 	Array(unsigned int n) : array_(new T[n]()), size_(n) {}
-	
 	~Array() {
 		if (array_ != NULL)
 			delete[] array_;
 	}
-	
 	Array(const Array &src) : array_(new T[src.size_]), size_(src.size_) {
 		for (unsigned int i = 0; i < size_; i++)
 			array_[i] = src.array_[i];
 	}
-	
 	Array &operator=(const Array &other) {
 		if (this != &other) {
 			delete[] array_;
@@ -32,14 +28,13 @@ class Array {
 	}
 	
 	T &operator[](unsigned int idx) {
-		if (idx >= size_ || idx < 0) {
+		if (idx >= size_) {
 			throw std::exception();
 		}
 		return array_[idx];
 	}
-	
 	const T &operator[](unsigned int idx) const {
-		if (idx >= size_ || idx < 0) {
+		if (idx >= size_) {
 			throw std::exception();
 		}
 		return array_[idx];
